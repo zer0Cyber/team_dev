bbee672c320dc6e570f7550944720470333b589b
taaako <ta12ko.sa@gmail.com>
2020-12-26 14:36:07 +0900

    ふくわらいの機能実装

--- fukuwarai/fukuwarai.js
+++ fukuwarai/fukuwarai.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
 });
 
 let i=0;
-let f=0;
 
 jQuery(function(){
     $("#face1").hide();
     $("#face2").hide();
     $("#face3").hide();
     $(".cover").hide();
-    //$(".cover2").hide();
 
     $("#facea").click(function(){
         $("#face1").show();
@@ -28,28 +26,17 @@ jQuery(function(){
         $("#face3").show();
     })
 
-    //for(let j=1; j < 10; j++){
-        //i++;
-        // $("#me"+j).mouseup(function(){
-        //     console.log(j);
-        //     $(".cover").css('z-index', 1002);
-        // }).mousedown(function(){
-        //     $(".cover").css('z-index', j);
-        //     $(".cover").show();
-        // })
-        $(".img").mousedown(function(){
-            if(i==0){
-                $(".cover").show();
-            }
-            $(".cover").css('z-index', 2);
-            i++;
-            console.log(i);
-        }).mouseup(function(){
-            $(".cover").css('z-index', 1002);
-            i++;
-            console.log(i);
-        })
-    //}
+    /*目隠し機能 */
+    $(".img").mousedown(function(){
+        if(i==0){
+            $(".cover").show();
+        }
+        $(".cover").css('z-index', 2);
+        i++;
+    }).mouseup(function(){
+        $(".cover").css('z-index', 1002);
+        i++;
+    })
 
     $("#finish").click(function(){
         $(".cover").hide();
