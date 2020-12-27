5175d3d625cbe67b1f8904fcbdab0e6a8cd3d7ee
katsumatarin <nir.k021017@gmail.com>
2020-12-27 14:54:08 +0900

    絵馬にtextareaを追加

--- ema/sample.js
+++ ema/sample.js
@@ -36,11 +36,13 @@ $(document).on('click', '#btn_ema_ok', function(){
   $('#img_ema3').remove();
   $('.img_btn_choice').append('<td><img src="img/ema_normal_ura.png" id="img_ema1"></td>')
   $("#setsumei").html("今年の抱負や願いを書きましょう！");
+  $('.ema_box').append('<form name="ematextarea"><textarea name="emabox" class="sample3" cols="40" rows="8"></textarea></form>');
   $('.ema_kakunin').append('<a class="btn btn-border-shadow btn-border-shadow--color2" id="btn_ema_kansei">完成！</a>')
 });
 $(document).on('click', '#btn_ema_back', function(){
 
 });
 $(document).on('click', '#btn_ema_kansei', function(){
-
+    const pray = document.ematextarea.emabox.value;
+    $('.ema_box').remove();
 });
